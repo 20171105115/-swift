1e690817c1fb098c1ea2f059b1d64a9bef704d04
20171105115 <40412857+20171105115@users.noreply.github.com>
2018-12-20 10:27:45 +0800

    堆栈计算机更新
    运用了截取字符串的操作，并且将操作数以及操作符号运用队列、堆栈的方法将中缀表达式转为后缀表达式进行运算，但是运行时还会崩溃。

--- 堆栈——swift/ViewController.swift
+++ 堆栈——swift/ViewController.swift
@@ -11,167 +11,71 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var number: UITextField!
+    
     var Flag = 0//判断前边是否为运算符 1是0不是
-    var Temp = 0.0//用于存储每个运算数
+    var Temp = ""//用于存储每个运算数
     @IBAction func NumberOne(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "1"
-        }else{
-            number.text = "1"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "1"
     }
     
     @IBAction func NumberTwo(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "2"
-        }else{
-            number.text = "2"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "2"
     }
     
     @IBAction func numberThree(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "3"
-        }else{
-            number.text = "3"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "3"
     }
     
     @IBAction func NumberFour(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "4"
-        }else{
-            number.text = "4"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "4"
     }
     
     @IBAction func NumberFive(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "5"
-        }else{
-            number.text = "5"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "5"
     }
     
     @IBAction func NumberSix(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "6"
-        }else{
-            number.text = "6"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "6"
     }
     
     @IBAction func NumberSeven(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "7"
-        }else{
-            number.text = "7"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "7"
     }
     
     @IBAction func NumberEight(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "8"
-        }else{
-            number.text = "8"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "8"
     }
     
     @IBAction func NumberNine(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "9"
-        }else{
-            number.text = "9"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
-    }
-    
-    @IBAction func NumberPlus(_ sender: Any) {
-        
-    }
-    
-    @IBAction func Numbersub(_ sender: Any) {
-        
+        number.text! += "9"
     }
     
     @IBAction func NumberZero(_ sender: Any) {
-        if Flag == 0{
-            number.text! += "0"
-        }else{
-            number.text = "0"
-        }
-        Flag = 0
-        Temp = Double(number.text!)!
+        number.text! += "0"
+    }
+    
+    @IBAction func NumberPlus(_ sender: Any) {
+        number.text! += "+"
+    }
+    
+    @IBAction func Numbersub(_ sender: Any) {
+        number.text! += "-"
     }
     
     @IBAction func NumberRide(_ sender: Any) {
-        
+        number.text! += "*"
     }
     
     @IBAction func NumberDivide(_ sender: Any) {
-        
+        number.text! += "/"
     }
     
     @IBAction func NumberPower(_ sender: Any) {
-        
-    }
-    
-    @IBAction func NumberEqual(_ sender: Any) {
-        
-    }
-    
-    
-    
-    var str = [String]()
-    var out = [String]()
-    var len:Int = 0
-    var cnt = 0
-    
-
-    
-    public struct Stack<String> {
-        fileprivate var array = [String]()
-        
-        public var isEmpty: Bool {
-            return array.isEmpty
-        }
-        
-        public var count: Int {
-            return array.count
-        }
-        
-        public mutating func push(_ element: String) {
-            array.append(element)
-        }
-        
-        public mutating func pop() -> String? {
-            return array.popLast()
-        }
-        
-        public func peek() -> String? {
-            return array.last
-        }
+        number.text! += "^"
     }
     
     public struct NumberStack<Int> {
-        fileprivate var array = [Double]()
+        fileprivate var array = [Int]()
         
         public var isEmpty: Bool {
             return array.isEmpty
@@ -181,31 +85,105 @@ class ViewController: UIViewController {
             return array.count as! Int
         }
         
-        public mutating func push(_ element: Double) {
+        public mutating func push(_ element: Int) {
             array.append(element)
         }
         
-        public mutating func pop() -> Double? {
+        public mutating func pop() -> Int? {
             return array.popLast()
         }
         
-        public func peek() -> Double? {
+        public func peek() -> Int? {
             return array.last
         }
     }
     
-    var st:Stack<String>?
-    var nst:NumberStack<Int>?
-    
-    func isnumber(ch:String) -> Bool {
-        if "0"<=ch&&ch<="9"{
-            return true
-        }else{
-            return false
+    public struct Queue<Character> {
+        // 数组用来存储数据元素
+        fileprivate var data = [Character]()
+        // 构造方法，用于构建一个空的队列
+        public init() {}
+
+        // 将类型为T的数据元素添加到队列的末尾
+        public mutating func enqueue(element: Character) {
+            data.append(element)
+        }
+        // 移除并返回队列中第一个元素
+        // 如果队列不为空，则返回队列中第一个类型为T的元素；否则，返回nil。
+        public mutating func dequeue() -> Character? {
+            return data.removeFirst()
+        }
+        // 返回队列中的第一个元素，但是这个元素不会从队列中删除
+        // 如果队列不为空，则返回队列中第一个类型为T的元素；否则，返回nil。
+        public func peek() -> Character? {
+            return data.first
+        }
+        // 清空队列中的数据元素
+        public mutating func clear() {
+            data.removeAll()
+        }
+        // 返回队列中数据元素的个数
+        public var count: Int {
+            return data.count
+        }
+        // 返回或者设置队列的存储空间
+        public var capacity: Int {
+            get {
+                return data.capacity
+            }
+            set {
+                data.reserveCapacity(newValue)
+            }
+        }
+        // 检查队列是否已满
+        // 如果队列已满，则返回true；否则，返回false
+        public func isFull() -> Bool {
+            return count == data.capacity
+        }
+        // 检查队列是否为空
+        // 如果队列为空，则返回true；否则，返回false
+        public func isEmpty() -> Bool {
+            return data.isEmpty
         }
     }
     
-    func priority(ch:String) -> Int {
+    
+    
+    public struct orderStack<Character> {
+        fileprivate var array = [Character]()
+        
+        public var isEmpty: Bool {
+            return array.isEmpty
+        }
+        
+        public var count: Int {
+            return array.count
+        }
+        
+        public mutating func push(_ element: Character) {
+            array.append(element)
+        }
+        
+        public mutating func pop() -> Character? {
+            return array.popLast()
+        }
+        
+        public func peek() -> Character? {
+            return array.last
+        }
+    }
+    
+   
+    //不包含后几个字符串的方法
+
+    
+
+    var nst = NumberStack<Int>()//存放数字的堆栈
+    var st = orderStack<Character>()//存放运算数的堆栈
+    var TheSuffix = Queue<Character>()  //存放后缀表达式的队列
+    
+    
+    func priority(ch:Character) -> Int {
         switch ch {
         case "(":
             return 0
@@ -224,61 +202,79 @@ class ViewController: UIViewController {
         }
     }
     
+    func isnumber(ch:Character) -> Bool {
+        if "0"<=ch && ch<="9" {
+            return true
+        }else{
+            return false
+        }
+    }
+    
     @IBAction func equal(_ sender: Any) {
         if (number.text?.isEmpty)! {
             print("无运算式，请重新输入\n")
         }else{
-            str = (number.text?.components(separatedBy: ""))!
-        }
-        len = str.count
-        for i in 0...len{
-            if isnumber(ch: str[i]){//如何将字符串中的单个字符放在out数组里面成为了一个难题
-                out[cnt] = str[i]
-                cnt += 1
-            }else{
-                    if str[i]=="("&&st!.isEmpty{
-                        st!.push(str[i])
+            for i in 0...(number.text)!.count-1{
+                let index = number.text!.index(number.text!.startIndex, offsetBy: i)
+                let temp = number.text![index]
+                if "0"<=number.text![index]&&number.text![index]<="9"{
+                    TheSuffix.enqueue(element: temp)
+                }else{
+                    if(temp=="("||st.isEmpty){
+                        st.push(temp)
                         continue
                     }
-                    if str[i]==")"{
-                        while(st!.peek() != "("){
-                            out[cnt] = st!.peek()!
-                            cnt += 1
-                            st!.pop()
+                    if(temp==")"){
+                        while(st.peek() != "("){
+                            TheSuffix.enqueue(element: st.pop()!)
                         }
-                        st!.pop()
+                        st.pop()
+                        continue
                     }
-                    while((st!.isEmpty != true)&&(st!.peek() != "(")&&(priority(ch: st!.peek()!))<(priority(ch: str[i]))){
-                        st!.push(st!.peek()!)
-                        st!.pop()
+                    while(!st.isEmpty&&st.peek() != "("&&priority(ch: temp) <= priority(ch :st.peek()!)){
+                        TheSuffix.enqueue(element: st.pop()!)
                     }
-                    st!.push(str[i])
+                    st.push(temp)
                 }
-        }
-    
-        out[cnt] = "\0"
-        var temp:Double = 0.0
-        for i in 0...cnt{
-            if isnumber(ch: out[i]){
-                nst!.push(Double(out[i])!)
-            }else if out[i] == "+"{
-                temp += nst!.peek()!
-                nst!.pop()
-            }else if out[i] == "-"{
-                temp -= nst!.peek()!
-                nst!.pop()
-            }else if out[i] == "*"{
-                temp *= nst!.peek()!
-                nst!.pop()
-            }else if out[i] == "/"{
-                temp /= nst!.peek()!
-                nst!.pop()
-            }else if out[i] == "^"{
-                pow(temp, nst!.peek()!)
-                nst!.pop()
             }
         }
-        number.text = "\(temp)"
+        var temp1 = 0  //记录临时的运算数
+        while !st.isEmpty {
+            TheSuffix.enqueue(element: st.pop()!)
+        }
+        for _ in 0...TheSuffix.count-1 {
+            if(isnumber(ch: TheSuffix.peek()!)){
+                nst.push(Int(String(TheSuffix.dequeue()!))!)
+                continue
+            }else if(TheSuffix.peek() == "+"){
+                TheSuffix.dequeue()
+                temp1 = nst.pop()!
+                temp1 = nst.pop()! + temp1
+                nst.push(temp1)
+            }else if(TheSuffix.peek() == "-"){
+                TheSuffix.dequeue()
+                temp1 = nst.pop()!
+                temp1 = nst.pop()! - temp1
+                nst.push(temp1)
+            }else if(TheSuffix.peek() == "*"){
+                TheSuffix.dequeue()
+                temp1 = nst.pop()!
+                temp1 = nst.pop()! * temp1
+                nst.push(temp1)
+            }else if(TheSuffix.peek() == "/"){
+                TheSuffix.dequeue()
+                temp1 = nst.pop()!
+                temp1 = nst.pop()! / temp1
+                nst.push(temp1)
+            }
+            /*else if(out[i]=="^"){
+                temp1 = nst.pop()!
+                temp1 = pow(nst.pop()!,temp1)
+                nst.push(temp1)
+            }*/
+            
+        }
+        number.text = "\(TheSuffix.dequeue()!)"
     }
     
 
